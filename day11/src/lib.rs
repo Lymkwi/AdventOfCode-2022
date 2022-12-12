fe9ba288b584e9e5e72ed65deeab95c832e9a4cf
Lux Amelia "Lymkwi" Phifollen <limefox@vulpinecitrus.info>
2022-12-12 06:14:59 +0000

    [day11] Part 1, 2 and tests

--- day11/src/lib.rs
+++ day11/src/lib.rs
@@ -13,14 +13,13 @@
 #![deny(missing_docs)]
 #![deny(rustdoc::missing_crate_level_docs)]
 
-// Change this on the day you start working on the puzzle
-#![allow(unused_variables)]
+use std::collections::{HashSet, VecDeque};
 
 /// Solve Advent of Code day 11 part one
 ///
 /// # Arguments
 ///
-///  - `data` : a `&str` that holds both numbers for today's input.
+///  - `data` : a `&str` that holds the input for today's puzzle
 ///
 /// # Return value
 ///
@@ -31,16 +30,176 @@
 ///
 /// If any conversion assumed to be valid with the input fails, panics.
 #[must_use]
-#[allow(clippy::missing_const_for_fn)]
 pub fn solve_part_one(data: &str) -> usize {
-    0
+	let mut monkeys = data.split("\n\n")
+		.map(|st| st.parse::<Monkey>().unwrap())
+		.collect::<Vec<Monkey>>();
+	// We run 20 cycles
+	for _ in 0..20 {
+		for idx in 0..monkeys.len() {
+			let base_monkey = monkeys.get_mut(idx).unwrap();
+			let div = base_monkey.get_div();
+			let out_monkeys = base_monkey.get_out_monkeys();
+			let op = base_monkey.get_op();
+			base_monkey.inspect_count_up();
+			let items = base_monkey.get_values_mut();
+			// Index of where stuff ends up
+			let mut out_list: Vec<(usize, usize)> = Vec::new();
+			for _ in 0..items.len() {
+				let v = op.apply(items.pop_front().unwrap()) / 3;
+				let out_monkey = if v % div == 0 {
+					out_monkeys.0
+				} else { out_monkeys.1 };
+				out_list.push((out_monkey, v));
+			}
+
+			// Empty the list
+			for (target, val) in out_list {
+				monkeys.get_mut(target).unwrap()
+					.get_values_mut().push_back(val);
+			}
+		}
+		//println!("{:?}", monkeys);
+	}
+	let mut counts = monkeys.iter()
+		.map(Monkey::get_inspect_count)
+		.collect::<Vec<usize>>();
+	counts.sort_unstable();
+	counts.reverse();
+	counts.first().unwrap() * counts.get(1).unwrap()
+}
+
+#[derive(Debug)]
+struct Monkey {
+	items: VecDeque<usize>,
+	out_monkeys: (usize, usize),
+	oper: Operation,
+	cond: usize,
+	inspect_count: usize,
+}
+
+#[derive(Debug,Copy,Clone)]
+enum Operand {
+	Val(usize),
+	Old
+}
+
+#[derive(Debug,Copy,Clone)]
+enum Operator {
+	Add,
+	Mul,
+	Sub
+}
+
+#[derive(Debug,Copy,Clone)]
+struct Operation(Operator, Operand);
+
+impl Operation {
+	const fn apply(&self, x: usize) -> usize {
+		match self.1 {
+			Operand::Old => {
+				match self.0 {
+					Operator::Add => x * 2,
+					Operator::Sub => 0,
+					Operator::Mul => x * x
+				}
+			},
+			Operand::Val(v) => {
+				match self.0 {
+					Operator::Add => x + v,
+					Operator::Sub => x - v,
+					Operator::Mul => x * v
+				}
+			}
+		}
+	}
+}
+
+impl Monkey {
+	fn get_values_mut(&mut self) -> &mut VecDeque<usize> {
+		&mut self.items
+	}
+
+	fn inspect_count_up(&mut self) {
+		self.inspect_count += self.items.len();
+	}
+
+	const fn get_inspect_count(&self) -> usize {
+		self.inspect_count
+	}
+
+	const fn get_op(&self) -> Operation {
+		self.oper
+	}
+
+	const fn get_div(&self) -> usize {
+		self.cond
+	}
+
+	const fn get_out_monkeys(&self) -> (usize, usize) {
+		self.out_monkeys
+	}
+}
+
+impl std::str::FromStr for Monkey {
+	type Err = ();
+	fn from_str(st: &str) -> Result<Self, Self::Err> {
+		// Alright, let's suffer now
+		let mut lines = st.split('\n').skip(1);
+		// The order will always be the same
+		let items: VecDeque<usize> = lines.next().unwrap()
+			.split(": ").nth(1).unwrap()
+			.split(", ")
+			.map(|s| s.parse::<usize>().unwrap())
+			.collect();
+		// The operation ***sucks***
+		let operation_str = lines.next().unwrap()
+			.split("new = ").nth(1).unwrap();
+		let mut operands = operation_str.split(' ').skip(1); // first one is
+															 // always "old"
+		let operator = match operands.next().unwrap() {
+			"+" => Operator::Add,
+			"-" => Operator::Sub,
+			"*" => Operator::Mul,
+			_ => unreachable!(),
+		};
+		let operand = match operands.next().unwrap() {
+			"old" => Operand::Old,
+			ns => {
+				let num = ns.parse::<usize>().unwrap();
+				Operand::Val(num)
+			}
+		};
+		let operation = Operation(operator, operand);
+
+		// Div factor
+		let div_factor = lines.next().unwrap()
+			.split(" by ").nth(1).unwrap()
+			.parse::<usize>().unwrap();
+
+		// Monkeys
+		let monkey_true = lines.next().unwrap()
+			.split("monkey ").nth(1).unwrap()
+			.parse::<usize>().unwrap();
+		let monkey_false = lines.next().unwrap()
+			.split("monkey ").nth(1).unwrap()
+			.parse::<usize>().unwrap();
+
+		Ok(Self {
+			out_monkeys: (monkey_true, monkey_false),
+			items,
+			oper: operation,
+			cond: div_factor,
+			inspect_count: 0,
+		})
+	}
 }
 
 /// Solve Advent of Code day 11 part two
 ///
 /// # Arguments
 ///
-///  - `data` : a `&str` that holds both numbers for today's input.
+///  - `data` : a `&str` that holds the input for today's puzzle
 ///
 /// # Return value
 ///
@@ -51,8 +210,56 @@ pub fn solve_part_one(data: &str) -> usize {
 ///
 /// If any conversion assumed to be valid with the input fails, panics.
 #[must_use]
-#[allow(clippy::missing_const_for_fn)]
 pub fn solve_part_two(data: &str) -> usize {
-    0
+	let mut monkeys = data.split("\n\n")
+		.map(|st| st.parse::<Monkey>().unwrap())
+		.collect::<Vec<Monkey>>();
+	// So because we no longer divide by three, nothing prevents us from having
+	// the numbers explode.
+	// Well, nothing save for... modular arithmetic!
+	// Since all of the modular division factors are prime, we can take the
+	// multiple of all of them to figure out the smallest cycle of numbers we
+	// need to keep track of
+
+	let lcm: usize = monkeys.iter()
+		.map(Monkey::get_div)
+		.collect::<HashSet<usize>>()
+		.iter()
+		.product();
+
+	// We run 10000 cycles
+	for _ in 0..10000 {
+		for idx in 0..monkeys.len() {
+			let base_monkey = monkeys.get_mut(idx).unwrap();
+			let div = base_monkey.get_div();
+			let out_monkeys = base_monkey.get_out_monkeys();
+			let op = base_monkey.get_op();
+			base_monkey.inspect_count_up();
+			let items = base_monkey.get_values_mut();
+			// Index of where stuff ends up
+			let mut out_list: Vec<(usize, usize)> = Vec::new();
+			for _ in 0..items.len() {
+				let v = op.apply(items.pop_front().unwrap()) % lcm;
+				let out_monkey = if v % div == 0 {
+					out_monkeys.0
+				} else { out_monkeys.1 };
+				out_list.push((out_monkey, v));
+			}
+
+			// Empty the list
+			for (target, val) in out_list {
+				monkeys.get_mut(target).unwrap()
+					.get_values_mut().push_back(val);
+			}
+		}
+		//println!("{:?}", monkeys);
+	}
+	let mut counts = monkeys.iter()
+		.map(Monkey::get_inspect_count)
+		.collect::<Vec<usize>>();
+	counts.sort_unstable();
+	counts.reverse();
+	counts.first().unwrap() * counts.get(1).unwrap()
 }
 
+// vim: set tw=80:
--- day11/src/main.rs
+++ day11/src/main.rs
@@ -8,18 +8,18 @@ use lib::{solve_part_one, solve_part_two};
 
 #[doc(hidden)]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let data = read_data("input")?;
-    println!("{}", solve_part_one(&data));
-    println!("{}", solve_part_two(&data));
-    Ok(())
+	let data = read_data("input")?;
+	println!("{}", solve_part_one(&data));
+	println!("{}", solve_part_two(&data));
+	Ok(())
 }
 
 
 #[cfg(test)]
 mod test {
-    use super::*;
-    use common::test;
+	use super::*;
+	use common::test;
 
-    test!(day11_01_example1, 1, 0, "");
-    test!(day11_02_example1, 2, 0, "");
+	test!(day11_01_example1, 1, 10605, "Monkey 0:\n  Starting items: 79, 98\n  Operation: new = old * 19\n  Test: divisible by 23\n    If true: throw to monkey 2\n    If false: throw to monkey 3\n\nMonkey 1:\n  Starting items: 54, 65, 75, 74\n  Operation: new = old + 6\n  Test: divisible by 19\n    If true: throw to monkey 2\n    If false: throw to monkey 0\n\nMonkey 2:\n  Starting items: 79, 60, 97\n  Operation: new = old * old\n  Test: divisible by 13\n    If true: throw to monkey 1\n    If false: throw to monkey 3\n\nMonkey 3:\n  Starting items: 74\n  Operation: new = old + 3\n  Test: divisible by 17\n    If true: throw to monkey 0\n    If false: throw to monkey 1");
+	test!(day11_02_example1, 2, 2713310158, "Monkey 0:\n  Starting items: 79, 98\n  Operation: new = old * 19\n  Test: divisible by 23\n    If true: throw to monkey 2\n    If false: throw to monkey 3\n\nMonkey 1:\n  Starting items: 54, 65, 75, 74\n  Operation: new = old + 6\n  Test: divisible by 19\n    If true: throw to monkey 2\n    If false: throw to monkey 0\n\nMonkey 2:\n  Starting items: 79, 60, 97\n  Operation: new = old * old\n  Test: divisible by 13\n    If true: throw to monkey 1\n    If false: throw to monkey 3\n\nMonkey 3:\n  Starting items: 74\n  Operation: new = old + 3\n  Test: divisible by 17\n    If true: throw to monkey 0\n    If false: throw to monkey 1");
 }
